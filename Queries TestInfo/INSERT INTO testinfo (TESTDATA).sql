debffd4d5757637577cf6ebe230d56a66ee93725
Edin <moralesedin01@gmail.com>
2023-01-23 14:13:57 -0500

    added more queries for each database, organized queries into separate folders

---
+++ Queries TestInfo/INSERT INTO testinfo (TESTDATA).sql
@@ -0,0 +1,13 @@
+INSERT INTO testinfo (id, firstName, lastName, email, startDate)
+VALUES 
+        (1,"Edin","Test","test1@gmail.com","2023-01-18"),
+        (2,"Morales","Test2","test2@gmail.com","2023-01-28"),
+        (3,"Antonio","Morales","antonio@gmail.com","2023-01-01"),
+        (4,"Allyson","Morales","allyson@gmail.com","2022-09-25"),
+        (5,"Anthony","Lopez","alopez@gmail.com","2023-01-15"),
+        (6,"Franklin","Ramirez","framirez@gmail.com","2023-01-04"),
+        (7,"Rosendo","Alvarez","ralvarez@gmail.com","2022-07-23"),
+        (8,"John","Titor","titor@gmail.com","2020-01-01"),
+        (9,"Kelvin","Martinez","martinez@gmail.com","2021-10-12"),
+        (10,"Anna","Torres","torres@gmail.com","2022-03-12")
+;
--- Queries/ADD COLUMN (basicInfo).sql
+++
@@ -1,1 +0,0 @@
-ALTER TABLE employee_database.basicinfo ADD COLUMN
