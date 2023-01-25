7432ef7f1c271cf8fdd926fa487a7922243bcfe1
Edin <moralesedin01@gmail.com>
2023-01-25 02:49:32 -0500

    added query for second table to merge with first table

---
+++ Queries TestInfo/Query Address table creation with filler data.sql
@@ -0,0 +1,17 @@
+CREATE TABLE addresses (
+    id INT PRIMARY KEY,
+    address VARCHAR(255)
+);
+
+INSERT INTO addresses (id, address)
+VALUES 
+(1, '123 Main St'),
+(2, '456 Park Ave'),
+(3, '789 Elm St'),
+(4, '321 Oak St'),
+(5, '159 Pine St'),
+(6, '753 Cedar St'),
+(7, '951 Birch St'),
+(8, '753 Maple St'),
+(9, '951 Willow St'),
+(10, '753 Oak St');
